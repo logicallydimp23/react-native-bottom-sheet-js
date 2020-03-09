b60105e92476157f80189532f6a620e7f77cc4af
John Steve Palero <jtpalero.halcyondigital@gmail.com>
2020-03-09 08:40:53 +0800

    initial

---
+++ index.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+
+import {
+  View,
+  StyleSheet,
+  UIManager,
+  LayoutAnimation,
+  Platform,
+  Dimensions,
+} from 'react-native'
+
+import { COLOR } from '@themes';
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    backgroundColor: COLOR.LIGHT,
+    position: 'absolute',
+    bottom: 0,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+  },
+  sheetContent: {
+    width: '100%',
+    minHeight: 56,
+    maxHeight: Dimensions.get('window').height / 2,
+  },
+})
+
+if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+class ActionSheet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    }
+  }
+
+  toggle = () => {
+    const { expanded } = this.state;
+    this.setState({ expanded: !expanded });
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+  }
+
+  open = data => {
+    this.setState({
+      expanded: true,
+    })
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
+  }
+
+  close = () => {
+    this.setState({ expanded: false });
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+  }
+
+  renderChildren = () => {
+    const { children } = this.props;
+    return (
+      <View style={styles.sheetContent}>
+        {children}
+      </View>
+    )
+  }
+
+  render() {
+    const {
+      expanded,
+      children
+    } = this.state;
+    return (
+      <View
+        style={[
+          styles.container,
+          // eslint-disable-next-line react-native/no-inline-styles
+          {
+            padding: expanded ? 20 : 0,
+          },
+        ]}
+      >
+        {expanded && children}
+      </View>
+    )
+  }
+}
+
+export default ActionSheet;
